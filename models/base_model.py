4dc818d18d6da024314ef68f360ee936d2678b0f
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-21 19:24:11 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,17 +10,24 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], time)
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], time)
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
