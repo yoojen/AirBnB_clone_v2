dbde67fa828880709355cde6981dcaa17500245e
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-08-23 00:39:45 -0700

    package

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,8 @@ storage_envirn = getenv("HBNB_TYPE_STORAGE")
 
 if storage_envirn == 'db':
     from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+        storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
