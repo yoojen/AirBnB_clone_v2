69ab05bbcc5221bd385775cb783c4ddc9f621901
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-23 09:34:30 +0200

    state updated

--- models/state.py
+++ models/state.py
@@ -7,11 +7,13 @@ import models
 
 
 class State(BaseModel, Base):
-    __tablename__ = 'states'
-    name = Column(String(128))
-    cities = relationship('City', backref='state')
     """ State class """
-    name = ""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state')
+    else:
+        name = ""
 
     @property
     def cities(self):
