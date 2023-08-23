341ba7a86e3e2c2b80d470d164ec7d4e446f1eb3
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-23 10:06:57 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -14,13 +14,17 @@ class State(BaseModel, Base):
         cities = relationship('City', backref='state')
     else:
         name = ""
+   def __init__(self, *args, **kwargs):
+    """initializes state"""
+    super().__init__(*args, **kwargs)
 
-    @property
-    def cities(self):
-        """returns all instances of city """
-        cities_obj = models.storage.all(City)
-        list_of_city = []
-        for city in cities_obj.values():
-            if city.state_id == self.id:
-                list_of_city.append(city)
-        return list_of_city
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """returns all instances of city """
+            cities_obj = models.storage.all(City)
+            list_of_city = []
+            for city in cities_obj.values():
+                if city.state_id == self.id:
+                    list_of_city.append(city)
+            return list_of_city
