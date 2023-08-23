335e233f8520de34b02e450e0ebf6ad6d208419a
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-23 09:21:30 +0200

    updated console

--- console.py
+++ console.py
@@ -215,22 +215,22 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
-    def do_all(self, arg):
-        """Prints string representations of instances"""
-        args = arg.split()
-        obj_list = []
-        if len(args) == 0:
-            obj_dict = storage.all()
-        elif args[0] in self.classes:
-            obj_dict = storage.all(self.classes[args[0]])
+    def do_all(self, args):
+        """ Shows all objects, or all objects of a class"""
+        print_list = []
+
+        if args:
+            args = args.split(' ')[0]  # remove possible trailing args
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for k, v in storage._FileStorage__objects.items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
         else:
-            print("** class doesn't exist **")
-            return False
-        for key in obj_dict:
-            obj_list.append(str(obj_dict[key]))
-        print("[", end="")
-        print(", ".join(obj_list), end="")
-        print("]")
+            for k, v in storage._FileStorage__objects.items():
+                print_list.append(str(v))
+
         print(print_list)
 
     def help_all(self):
