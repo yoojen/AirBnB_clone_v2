1eb8bc7b834f59ac4fb1112ccbc9e314cfeea2c4
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-23 09:30:10 +0200

    city updated

--- models/city.py
+++ models/city.py
@@ -2,12 +2,20 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
 
-class City(BaseModel):
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if models.storage_envirn == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        # places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ""
+        name = ""
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
