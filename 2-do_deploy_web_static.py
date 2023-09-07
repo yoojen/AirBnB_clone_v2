1c7a3647ee469e7214a7268df31252ee89da6826
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-09-07 03:16:17 -0700

    pycodestyle fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,8 +19,10 @@ def do_pack():
     local("tar -cvzf {} web_static".format(file_name))
     return file_name if path.exists(file_name) else None
 
+
 def do_deploy(archive_path):
-   """This function does deploy archive file to server"""
+    """This function does deploy archive file to server"""
+
     if archive_path is None or path.exists(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -28,22 +30,26 @@ def do_deploy(archive_path):
     upload_path = "/tmp/" + file
     if put(archive_path, upload_path).failed is True:
         return False
-    if run('mkdir -p /data/web_static/releases/' +filename + '/').failed is True:
+    if run('mkdir -p /data/web_static/releases/' + filename + '/\
+           ').failed is True:
         return False
-    #uncompressing to other folader
-    if run('tar -xvzf' + upload_path + '-C /data/web_static/releases/' + filename).failed is True:
+    # uncompressing to other folader
+    if run('tar -xvzf' + upload_path + ' -C /data/web_static/releases/\
+           ' + filename).failed is True:
         return False
-    #deleting compressed file from server
+    # deleting compressed file from server
     if run('rm -rf ' + upload_path).failed is True:
         return False
-    if run("mv /data/web_static/releases/" + filename + "/web_static/* /data/web_static/releases/"+ filename +"/").failed is True:
+    if run("mv /data/web_static/releases/" + filename + "/web_static/* \
+           /data/web_static/releases/" + filename + "/").failed is True:
         return False
-    if run("rm -rf data/web_static/releases/" + filename + "/web_static").failed is True:
+    if run("rm -rf data/web_static/releases/" + filename + "\
+            /web_static").failed is True:
         return False
     # deleting symbolic link
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/" + filename + "/ /data/web_static/current").failed is True:
+    if run("ln -s /data/web_static/releases/" + filename + "/\
+            /data/web_static/current").failed is True:
         return False
     return True
-    
