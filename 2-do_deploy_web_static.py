c341ea1e3dafaed2f7c052aa44b6b2c9039d4e4e
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-09-07 04:41:19 -0700

    hey

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,26 +30,23 @@ def do_deploy(archive_path):
     upload_path = "/tmp/" + file
     if put(archive_path, upload_path).failed is True:
         return False
-    if run('mkdir -p /data/web_static/releases/' + filename + '/\
+    if run('sudo mkdir -p /data/web_static/releases/' + filename + '/\
            ').failed is True:
         return False
     # uncompressing to other folader
-    if run('tar -xvzf' + upload_path + ' -C /data/web_static/releases/\
-           ' + filename).failed is True:
+    rz = run('sudo tar -xzf ' + upload_path + ' -C /data/web_static/releases/' + filename)
+    if rz.failed is True:
         return False
     # deleting compressed file from server
-    if run('rm -rf ' + upload_path).failed is True:
+    if run('sudo rm -rf ' + upload_path).failed is True:
         return False
-    if run("mv /data/web_static/releases/" + filename + "/web_static/* \
-           /data/web_static/releases/" + filename + "/").failed is True:
+    if run("mv /data/web_static/releases/" + filename + "/web_static/* /data/web_static/releases/" + filename + "/").failed is True:
         return False
-    if run("rm -rf data/web_static/releases/" + filename + "\
-            /web_static").failed is True:
+    if run("sudo rm -rf data/web_static/releases/" + filename + " /web_static").failed is True:
         return False
     # deleting symbolic link
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/" + filename + "/\
-            /data/web_static/current").failed is True:
+    if run("sudo ln -s /data/web_static/releases/" + filename + "/ /web_static/current").failed is True:
         return False
     return True
