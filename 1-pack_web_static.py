b06b9a70192e051f6e5c2e1c808f1c92f5584e7a
Eugene <140322568+yoojen@users.noreply.github.com>
2023-09-07 17:39:33 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,10 +11,11 @@ from datetime import datetime
 def do_pack():
     """This function does pcking al files to one tgz compressed file"""
 
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     file_name = "versions/web_static_{}.tgz".format(date)
-    if path.exists("versions") is False:
-        local("mkdir versions")
-    # change to tgz
-    local("tar -cvzf {} web_static".format(file_name))
-    return file_name if path.exists(file_name) else None
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file_name)).failed is True:
+        return None
+    return file
