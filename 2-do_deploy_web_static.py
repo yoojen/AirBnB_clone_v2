c0d9a165cd24a2770ca54a0a1966c4b0884d7c75
Eugene <140322568+yoojen@users.noreply.github.com>
2023-09-07 14:13:38 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,45 @@
 #!/usr/bin/python3
 """ put data to the server by using fabfile"""
-import os.path
-from fabric.api import env
-from fabric.api import put,run
+
+from fabric.api import local, run, put
+from os import path
+from datetime import datetime
 
 env.hosts = ["3.94.213.85", "34.227.93.137"]
 
+
+def do_pack():
+    local("mkdir -p version")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "version/web-static_{}.tgz".format(date)
+    # change to tgz
+    local("tar -cvzf {} web-static".format(file_name))
+    return file_name if path.exists(file_name) else None
+
+
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-        returns true or false
-    """
-    if os.path.isfile(archive_path) is False:
+    if archive_path is None or path.exists(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     filename = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    upload_path = "/tmp/" + file
+    if put(archive_path, upload_path).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(filename)).failed is True:
+    if run('mkdir -p /data/web_static/releases/' +filename + '/').failed is True:
         return False
-    if run('sudo mkdir -p /data/web_static/releases/' + filename + '/\
-           ').failed is True:
+    #uncompressing to other folader
+    if run('tar -xvzf' + upload_path + '-C /data/web_static/releases/' + filename).failed is True:
         return False
-    # uncompressing to other folader
-    rz = run('sudo tar -xzf ' + upload_path + ' -C /data/web_static/releases/' + filename)
-    if rz.failed is True:
+    #deleting compressed file from server
+    if run('rm -rf ' + upload_path).failed is True:
         return False
-    # deleting compressed file from server
-    if run('sudo rm -rf ' + upload_path).failed is True:
+    if run("mv /data/web_static/releases/" + filename + "/web_static/* /data/web_static/releases/"+ filename +"/").failed is True:
         return False
-    if run("mv /data/web_static/releases/" + filename + "/web_static/* /data/web_static/releases/" + filename + "/").failed is True:
-        return False
-    if run("sudo rm -rf data/web_static/releases/" + filename + " /web_static").failed is True:
+    if run("rm -rf data/web_static/releases/" + filename + "/web_static").failed is True:
         return False
     # deleting symbolic link
-    if run("sudo rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("sudo ln -s /data/web_static/releases/" + filename + "/ /web_static/current").failed is True:
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(filename)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, filename)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(filename, filename)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(file)).failed is True:
-        return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(filename)).failed is True:
+    if run("ln -s /data/web_static/releases/" + filename + "/ /data/web_static/current").failed is True:
         return False
     return True
